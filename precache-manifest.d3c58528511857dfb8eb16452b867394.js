35a123c0d636e7ce3ba0457e373f19e2e3558926
ericktorrs1 <ericktorrs90.et@gmail.com>
2019-07-20 17:24:43 -0500

    Updates

---
+++ precache-manifest.d3c58528511857dfb8eb16452b867394.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd33b70be49d1b465aae6eca8735773a",
+    "url": "/clickygame-github.com/index.html"
+  },
+  {
+    "revision": "97d659db3a55e5346241",
+    "url": "/clickygame-github.com/static/css/main.bc24d8dd.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame-github.com/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "97d659db3a55e5346241",
+    "url": "/clickygame-github.com/static/js/main.db608e7a.chunk.js"
+  },
+  {
+    "revision": "e0a310ea98f7cf7a20aa",
+    "url": "/clickygame-github.com/static/js/runtime~main.6153ade4.js"
+  }
+]);
